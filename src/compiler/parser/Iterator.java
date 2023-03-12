54a1c3c1b9662cb2475633da447488707d49b9c7
Amadej M <amadej.milicev99@gmail.com>
2023-03-12 21:50:04 +0100

    Iterator created

---
+++ src/compiler/parser/Iterator.java
@@ -0,0 +1,36 @@
+package compiler.parser;
+
+import java.util.List;
+
+import compiler.lexer.Symbol;
+import compiler.lexer.TokenType;
+
+public class Iterator {
+    private int index;
+    private List<Symbol> symbols;
+
+    public Iterator(List<Symbol> symbols) {
+        this.symbols = symbols;
+        this.index = 0;
+    }
+
+    public Symbol getSymbol() {
+        return symbols.get(index);
+    }
+
+    public TokenType getNext() {
+        return symbols.get(index).tokenType;
+    }
+
+    public TokenType getPrevious() {
+        return symbols.get(index--).tokenType;
+    }
+
+    public void skip() {
+        index++;
+    }
+
+    public boolean hasNext() {
+        return getNext() != null;
+    }
+}
